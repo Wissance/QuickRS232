f145d07aa5d83401f8cd2f2b7d1e489da43cd702
Ushakov Michael <um.nix.user@gmail.com>
2023-03-21 17:24:52 +0500

    Added hat ti design entity module

--- quick_rs232.v
+++ quick_rs232.v
@@ -1,5 +1,30 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Company:        Wissance (https://wissance.com)
+// Engineer:       EvilLord666 (Ushakov MV)
+// 
+// Create Date:    22/12/2022 
+// Design Name: 
+// Module Name:    quick_rs232
+// Project Name:   QuickRS232
+// Target Devices: Any
+// Tool versions:  Quartus Prime Lite 18.1
+// Description:    RS-232 интерфейс с поддержкой аппаратного управления потоком
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 1.0
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
 module quick_rs232 #(
+    INPUT_BUFFER_LEN = 16  // 
 )
-()
+(
+   input wire rx,
+   output wire tx,
+   output wire rts,
+   input wire cts,
+);
 
 endmodule
