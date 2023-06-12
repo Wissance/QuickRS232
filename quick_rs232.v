eb3e756df66ce7caf9bdf141c235d7b8da5a2a8d
Ushakov Michael <um.nix.user@gmail.com>
2023-06-12 23:25:59 +0500

    moving forward

--- quick_rs232.v
+++ quick_rs232.v
@@ -25,6 +25,11 @@
 `define ONE_STOP_BIT           0
 `define ONE_AND_HALF_STOP_BITS 1
 `define TWO_STOP_BITS          2
+// Flow controls
+`define NO_FLOW_CONTROL        0
+`define CTS_RTS_FLOW_CONTROL   1
+// Baud Rate
+
 
 module quick_rs232 #(
     DEFAULT_BYTE_LEN = 8,              // Возможные значения: - 5, 6, 7, 8, 9
@@ -32,6 +37,7 @@ module quick_rs232 #(
     DEFAULT_STOP_BITS = `ONE_STOP_BIT, // Число стоп-бит
     DEFAULT_BAUD_RATE = 9600,          // Скороть обмена бод = бит/с
     DEFAULT_RECV_BUFFER_LEN = 16       // Размер приемного буфера в байтах
+    DEFAULT_FLOW_CONTROL = `NO_FLOW_CONTROL
 )
 (
     // Global Signals
@@ -47,9 +53,11 @@ module quick_rs232 #(
     output reg rx_hshake_byte_received,
     input wire rx_hshake_next_byte_ready,
     //
-    input wire [DEFAULT_BYTE_LEN-1:0] tx_data,
-    input wire tx_hshake_next_byte_ready, // setting to 1 when we have something in tx_data
-    output reg tx_hshake_next_byte_send        // module set here 1 when tx_data was sent
+    input wire tx_transaction,                 //
+    input wire [DEFAULT_BYTE_LEN-1:0] tx_data, // data that should be send trough RS232
+    input wire tx_data_ready,                  // required: setting to 1 when new data is ready to send
+    output reg tx_data_copied,                 // short pulse means that data was copied _--_____--______
+    output reg tx_busy                         // tx notes that data is sending via RS232 or RS232 module awaiting flow-control synch
 );
 
 localparam reg [3:0] IDLE_EXCHANGE_STATE = 1;
@@ -63,7 +71,11 @@ localparam reg [3:0] SYNCH_STOP_EXCHANGE_STATE = 8;
 
 reg [3:0] tx_state;
 reg [3:0] rx_state;
+reg tx_rts;
+reg rx_rts;
+reg [DEFAULT_BYTE_LEN-1:0] tx_buffer;
 
+assign rts = tx_rts || rx_rts; // Hardware Flow Controll
 
 /*
  * Блок для чтения (rx) данных из RS232
@@ -74,6 +86,7 @@ begin
     begin
         // clear all data
         rx_state <= IDLE_EXCHANGE_STATE;
+        rx_rts <= 1'b0;
     end
     else
     begin
@@ -115,18 +128,52 @@ begin
     begin
         // clear all data
         tx_state <= IDLE_EXCHANGE_STATE;
+        tx_data_copied <= 1'b0;
+        tx_busy <= 1'b0;
+        tx_rts <= 1'b0;
+        tx_buffer <= 0;
     end
     else
     begin
         case (tx_state)
             IDLE_EXCHANGE_STATE:
             begin
+                if (tx_transaction == 1'b1)
+                begin
+                    tx_state <= SYNCH_WAIT_EXCHANGE_STATE;
+                    tx_buffer <= 0;
+                    tx_data_copied <= 1'b0;
+                    tx_busy <= 1'b0;
+                end
             end
             SYNCH_WAIT_EXCHANGE_STATE:
             begin
+                // FLOW control synchronization
+                if (DEFAULT_FLOW_CONTROL == `NO_FLOW_CONTROL)
+                begin
+                   state <= SYNCH_START_EXCHANGE_STATE;
+                end
+                else
+                begin
+                   if (DEFAULT_FLOW_CONTROL == `CTS_RTS_FLOW_CONTROL)
+                   begin
+                       // RTS + CTS
+                       // Here we assume that RTS wired with RTS, CTS with CTS, no crossing RTS->CTS, CTS->RTS
+                       // therefore is nothing to do in TX here
+                       state <= SYNCH_START_EXCHANGE_STATE;
+                   end
+                end
             end
             SYNCH_START_EXCHANGE_STATE:
             begin
+                // wait for data here and move to real tx when we have data
+                if (tx_data_ready == 1'b1)
+                begin
+                   state <= START_BIT_EXCHANGE_STATE;
+                   tx_buffer <= tx_data;
+                   tx_data_copied <= 1'b1;
+                   tx_busy <= 1'b1;
+                end
             end
             START_BIT_EXCHANGE_STATE:
             begin
