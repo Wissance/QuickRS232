959ab82c5f36f2efcc3a65af46f780501594743d
Ushakov Michael <um.nix.user@gmail.com>
2023-09-06 01:05:50 +0500

    async FIFO does not work as expected, going to synch

--- fifo.v
+++ fifo.v
@@ -3,7 +3,7 @@
 // Company:             Wissance (https://wissance.com)
 // Engineer:            EvilLord666 (Ushakov MV - https://github.com/EvilLord666)
 // 
-// Create Date:         09.10.2016
+// Create Date:         05.09.2023
 // Design Name:    
 // Module Name:         fifo
 // Project Name:        QuickRS232
@@ -24,11 +24,12 @@ module fifo #
     parameter DATA_WIDTH = 32
 )
 (
+    input wire  clk,
     input wire  enable,
     input wire  clear,
     output wire fifo_ready,
-    input wire  push_clock,
-    input wire  pop_clock,
+    input wire  push,
+    input wire  pop,
     input wire  [DATA_WIDTH - 1 : 0] in_data,
     output wire [DATA_WIDTH - 1 : 0] out_data,
     output wire popped_last,
@@ -41,13 +42,109 @@ module fifo #
     reg [15: 0] data_count;
     reg [15: 0] position;
     reg [15: 0] counter;
+    reg [2:0] fifo_state;
+ 
+    localparam reg [2:0] INITIAL_STATE = 1;
+    localparam reg [2:0] PUSH_STARTED = 2;
+    localparam reg [2:0] PUSH_FINISHED = 3;
+    localparam reg [2:0] POP_STARTED = 4;
+    localparam reg [2:0] POP_FINISHED = 5;
+    localparam reg [2:0] OPERATION_AWAITING = 6;
     
-    assign fifo_ready = enable && ~clear;
+    assign fifo_ready = enable && ~clear;   // candidate 4 remove in a synchronous FIFO
     assign out_data = buffer;
     assign pushed_last = pushed_last_value;
     assign popped_last = popped_last_value;
+
+    always@ (posedge clk)
+    begin
+        if (clear == 1'b1)
+        begin
+            fifo_state <= INITIAL_STATE;
+            for(counter = 0; counter < FIFO_SIZE; counter = counter + 1)
+                fifo_data[counter] <= 0;
+            position <= 0;
+            data_count <= 0;    
+            popped_last_value <= 1'b1;
+            pushed_last_value <= 1'b0;
+            buffer <= 0;
+        end
+        else
+        begin
+            case (fifo_state)
+                INITIAL_STATE:
+                begin
+                    fifo_state <= OPERATION_AWAITING;
+                end
+                OPERATION_AWAITING:
+                begin
+                    if (push == 1'b1)
+                    begin
+                        fifo_state <= PUSH_STARTED;
+                    end
+                    if (pop == 1'b1)
+                    begin
+                       fifo_state <= POP_STARTED;
+                    end
+                end
+                PUSH_STARTED:
+                begin
+                    if(data_count <= FIFO_SIZE)
+                    begin
+                        popped_last_value <= 0;
+                        fifo_data[position] <= in_data;
+                        position <= position + 1;    // position is an index of next item ...
+                        data_count <= data_count + 1;
+                        if(data_count == FIFO_SIZE)
+                        begin
+                            pushed_last_value <= 1;
+                        end
+                        else
+                        begin
+                            pushed_last_value <= 0;
+                        end
+                    end
+                    if (push == 1'b0)
+                    begin
+                        fifo_state <= PUSH_FINISHED;
+                    end
+                end
+                PUSH_FINISHED:
+                begin
+                    fifo_state <= OPERATION_AWAITING;
+                end
+                POP_STARTED:
+                begin
+                    if (data_count >= 1)
+                    begin
+                        buffer = fifo_data[0];
+                        data_count = data_count - 1;
+                        pushed_last_value = 0;
+                        for(counter = 0; counter < FIFO_SIZE - 1; counter = counter + 1)
+                            fifo_data[counter] <= fifo_data[counter + 1];
+                        fifo_data[FIFO_SIZE - 1] <= 0;
+                        position <= position - 1;
+                        popped_last_value <= position == 0;
+                    end
+                    else
+                    begin
+                        popped_last_value <= 1;
+                        buffer <= 0;
+                    end
+                    if (pop == 1'b0)
+                    begin
+                        fifo_state <= POP_FINISHED;
+                    end
+                end
+                POP_FINISHED:
+                begin
+                    fifo_state <= OPERATION_AWAITING;
+                end
+            endcase
+        end
+    end
     
-    always@ (posedge push_clock, posedge pop_clock, posedge clear)
+    /*always@ (posedge push_clock, posedge pop_clock, posedge clear)
     begin
         if(clear)
         begin
@@ -100,5 +197,5 @@ module fifo #
                 end
             end
         end
-    end   
+    end*/
 endmodule
--- quick_rs232.v
+++ quick_rs232.v
@@ -59,7 +59,8 @@ module quick_rs232 #(
     input wire [DEFAULT_BYTE_LEN-1:0] tx_data,           // data that should be send trough RS232
     input wire tx_data_ready,                            // required: setting to 1 when new data is ready to send
     output reg tx_data_copied,                           // short pulse means that data was copied _--_____--______--___
-    output reg tx_busy                                   // tx notes that data is sending via RS232 or RS232 module awaiting flow-control synch
+    output reg tx_busy,                                  // tx notes that data is sending via RS232 or RS232 module awaiting flow-control synch
+    output reg [7:0] debug_led_bus
 );
 
 localparam reg [3:0] IDLE_EXCHANGE_STATE = 1;
@@ -77,6 +78,7 @@ localparam reg [31:0] PARITY_ANALYZE_OFFSET = 16;
 
 reg [31:0] TICKS_PER_UART_BIT;                           // = CLK_FREQ / DEFAULT_BAUD_RATE;
 reg [31:0] HALF_TICKS_PER_UART_BIT;                      // = TICKS_PER_UART_BIT / 2;
+reg [31:0] TOTAL_RX_TIMEOUT;
 
 reg [3:0] tx_state;
 reg [3:0] rx_state;
@@ -89,6 +91,7 @@ reg [DEFAULT_BYTE_LEN-1:0] rx_buffer;
 wire rx_data_buffer_full;
 reg [31:0] rx_bit_counter;
 reg [31:0] rx_stop_bit_counter_limit;
+reg [31:0] rx_timeout;
 reg [3:0]  rx_data_bit_counter;
 reg rx_data_parity;
 integer i;
@@ -121,9 +124,21 @@ begin
         TICKS_PER_UART_BIT <= CLK_TICKS_PER_RS232_BIT;
         HALF_TICKS_PER_UART_BIT <= CLK_TICKS_PER_RS232_BIT / 2;
         j <= 0;
+        TOTAL_RX_TIMEOUT <= 6400; // ~ 9600 bit/s
+        rx_timeout <= 0;
+        debug_led_bus <= 8'b11111111;
     end
     else
     begin
+        if (rx_state > SYNCH_START_EXCHANGE_STATE)
+        begin
+            rx_timeout <= rx_timeout + 1;
+        end
+        if (rx_timeout == TOTAL_RX_TIMEOUT)
+        begin
+            rx_state <= SYNCH_WAIT_EXCHANGE_STATE;
+        end
+
         case (rx_state)
             IDLE_EXCHANGE_STATE:
             begin
@@ -132,6 +147,7 @@ begin
             end
             SYNCH_WAIT_EXCHANGE_STATE:
             begin
+                rx_err <= 1'b0;
                 if (DEFAULT_FLOW_CONTROL == `NO_FLOW_CONTROL)
                 begin
                     rx_state <= SYNCH_START_EXCHANGE_STATE;
@@ -159,6 +175,7 @@ begin
                 // catch start from 1 to 0
                 if (rx == 1'b0)
                 begin
+                    rx_timeout <= 0;
                     rx_state <= START_BIT_EXCHANGE_STATE;
                     rx_bit_counter <= 0;
                 end
@@ -178,28 +195,33 @@ begin
             begin
                 // wait all bytes ....
                 // RX sends as LSB from 0 to 7 bit
+                rx_bit_counter <= rx_bit_counter + 1;
+
                 if (rx_data_bit_counter == DEFAULT_BYTE_LEN)
                 begin
+                    rx_bit_counter <= 0;
                     rx_data_bit_counter <= 0;
                     rx_state <= PARITY_BIT_EXCHANGE_STATE;
                 end
+                else
+                begin
+                    if (rx_bit_counter == TICKS_PER_UART_BIT - 64)  // ensure that we read corrected bit value quite away from boards
+                    begin
+                        rx_buffer[rx_data_bit_counter] <= rx;
+                    end
                 
-                if (rx_bit_counter == 0)
-                begin
-                    rx_buffer[rx_data_bit_counter] <= rx;
-                end
-                rx_bit_counter <= rx_bit_counter + 1;
-                if (rx_bit_counter == TICKS_PER_UART_BIT)
-                begin
-                    rx_bit_counter <= 0;
-                    rx_data_bit_counter <= rx_data_bit_counter + 1;
+                    if (rx_bit_counter == TICKS_PER_UART_BIT)
+                    begin
+                        rx_bit_counter <= 0;
+                        rx_data_bit_counter <= rx_data_bit_counter + 1;
+                    end
                 end
             end
             PARITY_BIT_EXCHANGE_STATE:
             begin
                 rx_bit_counter <= rx_bit_counter + 1;
-					 if (rx_bit_counter == TICKS_PER_UART_BIT - PARITY_ANALYZE_OFFSET)
-					 begin
+                if (rx_bit_counter == TICKS_PER_UART_BIT - PARITY_ANALYZE_OFFSET)
+                begin
                     // check parity, if parity is bad generate error, don't store byte
                     case (DEFAULT_PARITY)
                         `NO_PARITY:
@@ -233,7 +255,7 @@ begin
                             rx_state <= PARITY_BIT_ANALYZE_STATE;
                         end
                     endcase
-					 end
+                end
             end
             PARITY_BIT_ANALYZE_STATE:
             begin
@@ -241,30 +263,32 @@ begin
                 begin
                     if (rx_data_parity != rx)
                     begin
-                        rx_err <= 1'b1;
+                        rx_err <= 1'b1; // 1
                     end
                 end
                 else
                 begin
                     if (rx_data_parity != ~rx)
                     begin
-                        rx_err <= 1'b1;
+                        rx_err <= 1'b1;  // 1
                     end
                 end
                 rx_state <= PARITY_REMANENCE_TIMEOUT_WAIT_STATE;
             end
-				PARITY_REMANENCE_TIMEOUT_WAIT_STATE:
-				begin
-				    rx_bit_counter <= rx_bit_counter + 1;
-					 if (rx_bit_counter == TICKS_PER_UART_BIT + 1)
-					 begin
-					     rx_state <= STOP_BITS_EXCHANGE_STATE;
-					 end
-					 if (rx_err == 1'b0)
+            PARITY_REMANENCE_TIMEOUT_WAIT_STATE:
+            begin
+                rx_bit_counter <= rx_bit_counter + 1;
+                if (rx_bit_counter == TICKS_PER_UART_BIT + 1)
+                begin
+                    rx_bit_counter <= 0;
+                    rx_state <= STOP_BITS_EXCHANGE_STATE;
+                end
+
+                if (rx_err == 1'b0)
                 begin
                     rx_byte_received <= 1'b1;
                 end
-				end
+            end
             STOP_BITS_EXCHANGE_STATE:
             begin
                 if (rx == 1'b1)
@@ -383,17 +407,20 @@ begin
             end
             DATA_BITS_EXCHANGE_STATE:
             begin
+                if (tx_data_bit_counter == DEFAULT_BYTE_LEN)
+                begin
+                    tx_state <= PARITY_BIT_EXCHANGE_STATE;
+                    tx_data_copied <= 1'b0;
+                end
+                else
+                begin
                 tx <= tx_buffer[tx_data_bit_counter];
                 tx_bit_counter <= tx_bit_counter + 1;
                 if (tx_bit_counter == TICKS_PER_UART_BIT)
                 begin
                     tx_bit_counter <= 0;
                     tx_data_bit_counter <= tx_data_bit_counter + 1;
-                    if (tx_data_bit_counter == DEFAULT_BYTE_LEN - 1)
-                    begin
-                        tx_state <= PARITY_BIT_EXCHANGE_STATE;
-                        tx_data_copied <= 1'b0;
-                    end
+                end
                 end
             end
             PARITY_BIT_EXCHANGE_STATE:
